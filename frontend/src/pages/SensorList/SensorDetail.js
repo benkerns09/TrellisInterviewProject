0c033ca4b244ffb279ea5dcea226f920b76db08c
Ben Kerns <kernsb6@gmail.com>
2018-08-09 04:38:30 -0400

    add notes, remove notes, view notes

--- frontend/src/pages/SensorList/SensorDetail.js
+++ frontend/src/pages/SensorList/SensorDetail.js
@@ -1,22 +1,81 @@
 import React, { Component } from 'react';
-
+import NoteItems from './NoteItems';
+import "./SensorDetail.css";
 import { getSensor } from '../../services/SensorService';//function that fetch's sensors. Unused currently
 
 class SensorDetail extends Component {
-    componentDidMount() {
-        // fetch data for this.props.sensorId and set to state
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
+
+    addItem(e) {
+        var itemArray = this.state.items;
+
+        if (this._inputElement.value !== "") {
+            itemArray.unshift({
+                text: this._inputElement.value,
+                key: Date.now()
+            });
+
+            this.setState({
+                items: itemArray
+            });
+
+            this._inputElement.value = "";
+        }
+
+        console.log(itemArray);
+
+        e.preventDefault();
+    }
+    // componentDidMount() {
+    //     // fetch data for this.props.sensorId and set to state
+    // }
+
+
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function(item) {
+            return (item.key !== key);
+        });
+
+        this.setState({
+            items: filteredItems
+        });
+    }
+
 
     render() {
         return(
-            <div>
+            <div className="notesList">
                 <h1>Notes</h1>
                 {/* //this is what you see as dropdown option */}
                 {/* <p>{this.props.sensorId}</p> */}
                 {/* This grabs the ID for each sensor on the dropdown */}
-                <p>{this.props.sensorNotes}</p>
-
+                <form onSubmit={this.addItem}>
+                    <input classname="input" ref={(a) => this._inputElement = a}
+                        placeholder="Enter Note">
+                    </input>
+                    <button type="submit" className="note">Add Note</button>
+                </form>
+                {/* <p>{this.props.sensorNotes}</p> */}
+                {/* <form action='/sensor/:notes' method="post">
+                    <label>
+                        New Note:
+                        <input type="text" name="name" />
+                    </label>
+                    <button type="submit">Submit</button>
+                </form> */}
+                <NoteItems entries={this.state.items}
+                    delete={this.deleteItem} />
             </div>
+            
+            
         ) 
 
     }
--- frontend/src/services/SensorService.js
+++ frontend/src/services/SensorService.js
@@ -1,7 +1,7 @@
 const serverURL = 'http://localhost:9000';//connects to backend port
 
 export const getSensors = () => {//fuction to retrieve sensors from backend
-  return fetch(`${serverURL}/potato`)//fetching this from servers backend
+  return fetch(`${serverURL}/sensors`)//fetching this from servers backend
     .then(res => {
       if(res.status !== 200) {
         throw new Error('Error fetching sensors');
@@ -10,11 +10,11 @@ export const getSensors = () => {//fuction to retrieve sensors from backend
     });
 }
 
-export const getSensor = (id) => {//fuction to retrieve sensors from backend
-  return fetch(`${serverURL}/sensor/${id}`)//fetching this from servers backend
+export const getSensor = (notes) => {//fuction to retrieve sensors from backend
+  return fetch(`${serverURL}/sensor/${notes}`)//fetching this from servers backend
     .then(res => {
       if(res.status !== 200) {
-        throw new Error('Error fetching sensor with id: '+id);
+        throw new Error('Error fetching sensor with id: '+notes);
       }
       return res.json();//returns javascript object notation
     });
